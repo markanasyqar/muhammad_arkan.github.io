b3416658e3f0e3dc609a6255d0da4e3208381ca0
Muhammad Arkan <markanasyqar0704@gmail.com>
2024-10-22 10:22:12 +0700

    portfolio arkan

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '40px',
+    },
+    extend: {
+      colors: {
+        primary: '#2dd4bf',
+        secondary: '#475569',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
